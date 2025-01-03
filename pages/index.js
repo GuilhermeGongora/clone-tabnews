69fe0a5257658386b7d218a3f8d6cd626784831c
GuilhermeGongora <138256246+GuilhermeGongora@users.noreply.github.com>
2025-01-03 01:30:19 +0000

    deleção de funções no `Index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Mensagem impactante! 😎</h1>;
 }
 
-function teste() {
-  console.log("aaaaaaaaaa");
-}
-
-function teste2() {
-  console.log("Errado");
-}
-
 export default Home;
